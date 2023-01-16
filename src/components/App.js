3497a3dbdfe88f6833de84275cc13965840165ca
kennedy <kenneyrit@gmail.com>
2023-01-16 05:22:49 +0300

    Updated respective components

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,26 @@
 import React from "react";
-import blogData from "../data/blog";
 
-console.log(blogData);
-
-function App() {
+function minutesToRead(minutes) {
+  if (minutes > 0 && minutes < 30) {
+    const times = Math.ceil(minutes / 5);
+    return "☕️".repeat(times);
+  } else if (minutes >= 30) {
+    const times = Math.ceil(minutes / 10);
+    return "🍱".repeat(times);
+  } else {
+    return "";
+  }
+}
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
   return (
-    <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
-    </div>
+    <article>
+      <h3>{title}</h3>
+      <small>
+        {date} {minutesToRead(minutes)}
+        {minutes} min read
+      </small>
+      <p>{preview}</p>
+    </article>
   );
 }
-
-export default App;
+export default Article;
